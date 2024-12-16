68c8ef1294cb081a9e232077f40baf7e1dc042e7
TridentifyIshaan <ishaanrastogi19@gmail.com>
2024-12-16 16:37:55 +0530

    modified:   Files/L1/J4.class 	modified:   Files/L1/J4.java

--- Files/L1/J4.java
+++ Files/L1/J4.java
@@ -8,6 +8,7 @@
   
 */
 
+import java.util.Scanner; // Importing Scanner class from java.util package
 public class J4 { // Class name should be same as the file name    
     public static void main(String[] args) { //first method / function to be called by JVM
         //primitive datatypes are the datatypes which can't be further divided into more datatypes
@@ -29,6 +30,18 @@ public class J4 { // Class name should be same as the file name
         System.out.println("The large integer is " + largeInteger);
         System.out.println("The boolean value is " + check);
         System.out.println("The name is " + name);
+
+        // If a number is large and you don't want to forget how many zeroes are there, you use comma. Eg- 234, 000, 718 but here commas can't be used so instead you can use _ so that you don't forget how many zeroes are there. You may find it lame but it is actually a good trick to keep in mind.
+        int a = 234_000_718;
+        System.out.println(a);
+
+        //Taking input from the user
+        
+        Scanner input = new Scanner(System.in);
+        // float marks = input.nextFloat();
+        // This will give an error because we have already declared marks above. So, we can't declare it again.
+        float userMarks = input.nextFloat();
+        System.out.println("The marks entered by the user are " + userMarks);
 
       }
 }
