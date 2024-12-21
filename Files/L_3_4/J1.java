45900c00dfd279a9f2de84e778c23ea0545e6103
TridentifyIshaan <ishaanrastogi19@gmail.com>
2024-12-21 14:24:39 +0530

    new file:   Files/L_3_4/J1.java 	new file:   Files/L_3_4/J2.java

---
+++ Files/L_3_4/J1.java
@@ -0,0 +1,28 @@
+/*
+  
+  This note will be available only on first file of each folder present in folder Java .
+  Documentation aka basic notes on how to run the code is already kept in each file for each programme using (/) or //.
+  Wherever author needs it to be inserted, he has inserted. So that people using these files can learn easily how to code in Java Language. Author has made these programmes as a reference using courses of Kunal Kushwaha, while himself learning Java Language for the first time.
+  
+  > Date Created: December 21, 2024
+  > Author: Ishaan Rastogi
+  > Purpose: To 
+  > Operating System: This is only for Windows OS, it may or may not work on other OS
+  > Program Status: 100% Working
+  
+*/
+
+import java.util.Scanner;
+
+public class J2 {
+    public static void main(String[] args) {
+    }
+}
+
+/*
+  
+  For Terminal Code... Ctrl + Shift + `
+  javac filename.java
+  java filename
+  
+*/
---
+++ Files/L_3_4/J2.java
@@ -0,0 +1,65 @@
+/*
+
+  > Date Created: December 21, 2024
+  > Author: Ishaan Rastogi
+  > Purpose: To create calculator using switch case
+  > Operating System: This is only for Windows OS, it may or may not work on other OS
+  > Program Status: 100% Working
+  
+*/
+
+import java.util.Scanner;
+
+public class J2 {
+    public static void main(String[] args) {
+
+        // Scanning the input
+        Scanner input = new Scanner(System.in);
+        
+        // Taking the input from user till user doesn't press X or x.
+        // infinite loop
+        while (true) {
+            
+            System.out.print("Enter the operator: ");
+            char op = input.next().trim().charAt(0);
+            
+            if (op == 'x' || op == 'X') {
+                System.out.println("User has exited the calculator");
+                break;
+            }
+
+            if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
+                System.out.print("Enter the numbers to perform the operation: ");
+                int a = input.nextInt();
+                int b = input.nextInt();
+
+                if (op == '+') {
+                    System.out.println("The sum is: " + (a + b));
+                    
+                } else if (op == '-') {
+                    System.out.println("The difference is: " + (a - b));
+                    
+                } else if (op == '*') {
+                    System.out.println("The product is: " + (a * b));
+                    
+                } else if (op == '/') {
+                    System.out.println("The division is: " + (a / b));
+                    
+                } else if (op == '%') {
+                    System.out.println("The remainder is: " + (a % b));
+                    
+                }
+            } else {
+                System.out.println("Invalid Operator");
+            }
+        }
+    }
+}
+
+/*
+  
+  For Terminal Code... Ctrl + Shift + `
+  javac filename.java
+  java filename
+  
+*/
